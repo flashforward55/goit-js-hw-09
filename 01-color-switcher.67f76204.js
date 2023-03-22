61310261c94bd7575edc409e30810a92047ebeb6
flashforward55 <116800576+flashforward55@users.noreply.github.com>
2023-03-22 18:55:43 +0000

    Deploying to gh-pages from @ flashforward55/goit-js-hw-09@f4e61498db9ffd09f1ccaa620e91c35b1475962b 🚀

---
+++ 01-color-switcher.67f76204.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]"),document.querySelector("[data-stop]"),document.body;
+//# sourceMappingURL=01-color-switcher.67f76204.js.map
---
+++ 01-color-switcher.6ac1e9c7.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]"),document.querySelector("[data-stop]"),document.body;
+//# sourceMappingURL=01-color-switcher.6ac1e9c7.js.map

a71fcb4653c9028e285eab96558af8be9be58572
Anton Styazhkin <rudana089@gmail.com>
2023-03-23 19:26:31 +0200

    Get HTML elements

---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Get HTML elements
+const datePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('button[data-start]');
+const daysValue = document.querySelector('span[data-days]');
+const hoursValue = document.querySelector('span[data-hours]');
+const minutesValue = document.querySelector('span[data-minutes]');
+const secondsValue = document.querySelector('span[data-seconds]');
+startButton.disabled = true;

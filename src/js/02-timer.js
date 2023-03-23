52e450aeb8bff4d6ba20440be8b9c38988c61b4b
Anton Styazhkin <rudana089@gmail.com>
2023-03-23 19:40:58 +0200

    Initialize flatpickr date picker

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,3 +10,21 @@ const hoursValue = document.querySelector('span[data-hours]');
 const minutesValue = document.querySelector('span[data-minutes]');
 const secondsValue = document.querySelector('span[data-seconds]');
 startButton.disabled = true;
+
+// Initialize flatpickr date picker
+flatpickr(datePicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+    if (selectedDate < currentDate) {
+      Notify.failure('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+});

fc0f0a4a5c9f59b6ee1ef2f1a2e097d00651f4aa
flashforward55 <116800576+flashforward55@users.noreply.github.com>
2023-03-26 09:25:45 +0000

    Deploying to gh-pages from @ flashforward55/goit-js-hw-09@862b431e80f693d16e6400077bff651fd7f2b54e 🚀

---
+++ 01-color-switcher.3eae355e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.body;e.disabled=!0;var n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.3eae355e.js.map
